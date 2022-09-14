c0a9fb1dcf48b8eb89c4d95dcec48afaba44e466
Sebastian Zaleski <Sebastian_Zaleski@epam.com>
2022-09-14 15:26:40 +0200

    added rate-limiting by IP

--- src/main/java/com/szaleski/xmcy/CryptoRecommendationApplication.java
+++ src/main/java/com/szaleski/xmcy/CryptoRecommendationApplication.java
@@ -2,8 +2,10 @@ package com.szaleski.xmcy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CryptoRecommendationApplication {
 
 	public static void main(String[] args) {
